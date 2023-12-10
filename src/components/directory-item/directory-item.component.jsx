432a08b062eaaa21a4753af8f7403e28354fd831
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-12-10 12:57:37 +0000

    Main functionality completed
    css styles improvements will be done next and payment method needs to be added.

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./directory-item.styles.scss";
+
+const DirectoryItem = ({ category }) => {
+  const { title, imageUrl } = category;
+
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <Link className="body" to={`shop/${title}`}>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Link>
+    </div>
+  );
+};
+
+export default DirectoryItem;
